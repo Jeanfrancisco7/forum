8352fb9f478f5487f248db69ef55e80d0ed97ed6
Jeanfrancisco <jeanfrancisco7@gmail.com>
2022-11-07 05:53:38 -0300

    Proximas aulas

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,38 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.form.TopicoForm;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/topicos")
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    //@RequestMapping(value = "/topicos", method = RequestMethod.GET)
+    @GetMapping
+    public List<TopicoDto> lista(String nomeCurso){
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }else{
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
+    }
+
+    @PostMapping
+    public void cadastrar(TopicoForm topico){
+
+    }
+}
---
+++ src/main/java/br/com/alura/forum/form/TopicoForm.java
@@ -0,0 +1,7 @@
+package br.com.alura.forum.form;
+
+public class TopicoForm {
+    private String titulo;
+    private String mesagem;
+    private String nomeCurso;
+}
